1b14c52d1c41eceb31a2ae2d45f99849f3c77f90
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2023-01-16 16:00:04 +0200

    Improve descriptions and responsive design.

--- src/dark-mode.js
+++ src/dark-mode.js
@@ -1,5 +1,5 @@
-var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
 // Change the icons inside the button based on previous settings
 if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
@@ -8,7 +8,7 @@ if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localS
     themeToggleDarkIcon.classList.remove('hidden');
 }
 
-var themeToggleBtn = document.getElementById('theme-toggle');
+const themeToggleBtn = document.getElementById('theme-toggle');
 
 let event = new Event('dark-mode');
 
